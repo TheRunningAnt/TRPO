08df6ceb55cf9b8c9d29e006f30583b836bde11b
Alexey <TheRunningAnt.Live@gmail.com>
2017-10-05 02:01:06 +0700

    Add player_turn function. Add matrix

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -4,6 +4,12 @@
 
 #define SPACE ' '
 
+char matrix[3][3] = {
+    {SPACE, SPACE, SPACE},
+    {SPACE, SPACE, SPACE},
+    {SPACE, SPACE, SPACE}
+};
+
 void computer_turn()
 {
     register int t;
@@ -23,5 +29,29 @@ void computer_turn()
     else
     {
         *p = 'O';
+    }
+}
+
+void player_turn()
+{
+    int x, y;
+
+    printf("Ваш ход:\n")
+    printf("X: ");
+    scanf("%d", &x);
+    printf("Y: ");
+    scanf("%d", &y);
+
+    x--;
+    y--;
+
+    if(x < 0 || y < 0 || x > 2 || y > 2 || matrix[x][y] != SPACE)
+    {
+        printf("Неправильный ход, попробуйте ещё раз\n");
+        player_turn();
+    }
+    else
+    {
+        matrix[x][y] = 'X';
     }
 }
