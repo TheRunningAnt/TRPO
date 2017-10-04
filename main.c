726742845c2f7dbb1d78240f69433c2e0db5954b
Alexey <TheRunningAnt.Live@gmail.com>
2017-10-05 03:35:48 +0700

    Tic-tac-toe greeting

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char done;
+    done = SPACE;
+
+    printf("\n-----------------\n");
+    printf("Крестики-нолики");
+    printf("\n-----------------\n");
+
+
+}
